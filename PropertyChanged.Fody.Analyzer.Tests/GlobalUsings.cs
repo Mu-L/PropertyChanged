812227ed6eb1e2e032f16c720ce4286e03cd4cd5
tom-englert <thomas.englert@avl.com>
2026-07-03 14:27:09 +0200

    - tests use net10 target framework
    - migrate to CPM
    - update dependencies

--- PropertyChanged.Fody.Analyzer.Tests/GlobalUsings.cs
+++ PropertyChanged.Fody.Analyzer.Tests/GlobalUsings.cs
@@ -2,6 +2,5 @@
 global using AnalyzerTesting.CSharp.Extensions;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.Testing;
-global using Microsoft.CodeAnalysis.Testing.Verifiers;
 global using Microsoft.CodeAnalysis.Text;
 global using PropertyChanged;
